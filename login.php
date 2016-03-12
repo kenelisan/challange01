7776d82cff8f6ec780e1b6f963adcbf9937b8ab9
KEN <KEN>
2016-03-12 20:16:18 +0400

    KENAN&HEYDER&MURAD

--- login.php
+++ login.php
@@ -1,9 +1,25 @@
 <?php
-
+     
     include 'dbconnect.php';
-
+    session_start();
     $lemail = $_POST['lemail'];
     $lpass = $_POST['lpassword'];
+    $sql = "SELECT * FROM users WHERE user_email"; 
+    $sqli = "SELECT * FROM users WHERE password"; 
+    $result = mysqli_query($connect, "SELECT * FROM users WHERE user_email = '$lemail'&& password ='$lpass'" );
+echo "<br>";
+    $say = mysqli_num_rows($result);
+    if($say>0)
+    {
+        $hamisi = mysqli_fetch_array($result);
+        var_dump($hamisi);
+        $_SESSION ["login"]==true;
+        $_SESSION ["email"] = $lemail;
+        $_SESSION ["pass"]=$lpass;
+        
+        echo $_SESSION["email"]." ".$_SESSION["pass"]." ".$_SESSION["name"];
+        echo $_SESSION["name"]=$hamisi["user_name"];
+    }
 
     
 ?>
--- registr.php
+++ registr.php
@@ -6,32 +6,29 @@
     $ulname = $_POST['lastname'];
     $uemail = $_POST['email'];
     $upass = $_POST['password'];
-    
-
-
-    $sqli = "SELECT * FROM users WHERE user_email LIKE '%{$uemail}%'";
+    $sqli = 'SELECT user_email FROM users';
                 $check = mysqli_query($connect,$sqli);
+    
+    $sql = "INSERT INTO users(user_name,user_surname,user_email,password) VALUES ('$uname','$ulname','$uemail','$upass')";
 
-                $row=mysqli_fetch_array($check);
-                    
-                        if(empty($row)){
-                           
-                            header('Location:index.php');
+
+                $row = mysqli_fetch_assoc($check);
+                    foreach($row as $value){
+                        
+                    }
+                        if($uemail == $value){
+                            header('Location: congrat.php');
+                               mysqli_query($connect,$sql);
                         }else{
                             
-            $sql = "INSERT INTO users(user_name,user_surname,user_email,password) VALUES ('$uname','$ulname','$uemail','$upass')";
-            
-            header('Location:congrat.php');
-                            
-                            
+
+    
+                           header('Location: index.php');
                         }
                     
                 
 
 
-            $query = mysqli_query($connect,$sql);
     
-
-
    
 ?>
